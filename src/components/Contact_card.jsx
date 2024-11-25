7cb5c9a3b635c4432c982fa9c934e05812104e21
noahmorrow078 <noah@clubhausagency.com>
2024-11-25 14:53:38 -0600

    updated form to use jotform instead

--- src/components/Contact_card.jsx
+++ src/components/Contact_card.jsx
@@ -1,84 +1,52 @@
-import { Form, Input, TextArea, Button } from 'semantic-ui-react';
+import React, { useState, useEffect } from 'react';
 import '../form.css';
-import emailjs from 'emailjs-com'
-import Swal from 'sweetalert2'
-import React, { lazy } from 'react';
 
+const ContactCard = ({ formId = '243296452837062' }) => {
+  const [iframeHeight, setIframeHeight] = useState(465);
 
-
-const SERVICE_ID = "service_2yj20kv";
-const TEMPLATE_ID = 'template_qrup6gm';
-const USER_ID = "ZRU2U_UuG_DkM0FLP"
-
-
-const Contact_card = () => {
-    const handleOnSubmit = (e) => {
-      e.preventDefault();
-      emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
-        .then((result) => {
-          console.log(result.text);
-          Swal.fire({
-            icon: 'success',
-            title: 'Message Sent Successfully'
-          })
-        }, (error) => {
-          console.log(error.text);
-          Swal.fire({
-            icon: 'error',
-            title: 'Ooops, something went wrong',
-            text: error.text,
-          })
-        });
-      e.target.reset()
+  useEffect(() => {
+    const handleMessage = (event) => {
+      // Ensure the message is from JotForm
+      if (event.origin === 'https://form.jotform.com') {
+        try {
+          const data = JSON.parse(event.data);
+          if (data.type === 'setHeight') {
+            setIframeHeight(data.height);
+          }
+        } catch (error) {
+          console.error('Error parsing message:', error);
+        }
+      }
     };
 
+    // Add message event listener
+    window.addEventListener('message', handleMessage);
 
-    return (    
-   
+    // Cleanup listener on component unmount
+    return () => {
+      window.removeEventListener('message', handleMessage);
+    };
+  }, []);
 
-      
-
-            <div className="contact_card">
-      <Form onSubmit={handleOnSubmit}>
-        <Form.Field
-          id='form-input-control-email'
-          control={Input}
-          label='Email'
-          name='user_email'
-          placeholder='Email…'
-          required
-          icon='mail'
-          iconPosition='left'
-        />
-        <Form.Field
-          id='form-input-control-last-name'
-          control={Input}
-          label='Name'
-          name='user_name'
-          placeholder='Name…'
-          required
-          icon='user circle'
-          iconPosition='left'
-        />
-        <Form.Field
-          id='form-textarea-control-opinion'
-          control={TextArea}
-          label='Message'
-          name='user_message'
-          placeholder='Message…'
-          required
-        />
-        <Button type='submit' color='green'>Submit</Button>
-        <div className="social-icons">
-          <a  target="_blank" href="https://www.facebook.com/ClubHaus-Agency-101800182526646"><img src="./images/icons/facebook.png"/></a>
-          <a target="_blank" href="https://www.linkedin.com/company/clubhaus-agency/"><img src="./images/icons/linkdin.png"/></a>
-        </div>
-      </Form>
+  return (
+    <div className="contact_card">
+      <iframe 
+        id={`JotFormIFrame-${formId}`}
+        title="Form"
+        src={`https://form.jotform.com/${formId}`}
+        allow="geolocation; microphone; camera; fullscreen"
+        allowTransparency={true}
+        frameBorder="0"
+        style={{
+          minWidth: '100%',
+          maxWidth: '100%',
+          height: `${iframeHeight}px`,
+          border: 'none'
+        }}
+        scrolling="no"
+      />
     </div>
+  );
+};
 
-
-     
-        )
-    }
-
-    export default Contact_card;
+export default ContactCard;
